2f68cfe03bb47fe9e00edfc244cfda0721de2f62
bastian <k.batakovich@gmail.com>
2019-08-08 23:14:39 +0300

    Refactor component to stateless

--- books-spa/src/header/header.jsx
+++ books-spa/src/header/header.jsx
@@ -1,45 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import header from './header.css';
 
 
-class Header extends Component {
-	/* принимаем обработчик из родительского компонента */
-	logOut = () => {
-		const { logOut } = this.props;
-		logOut();
-	};
-
-	render() {
-		const { isLogged } = this.props;
-		return (
-			<header>
-				<div className={header.layout}>
-					<div className={header.container}>
-						<nav className={header.nav}>
-							<ul className={header.nav_items}>
-								<li>
-									<NavLink to="/books" className={header.navlink} activeClassName={header.active}>Книги</NavLink>
-								</li>
-								<li>
-									<NavLink to="/authors" className={header.navlink} activeClassName={header.active}>Авторы</NavLink>
-								</li>
-								<li>
-									<NavLink to="/profile" className={header.navlink} activeClassName={header.active}>Профиль</NavLink>
-								</li>
-								<li>
-									{isLogged ? <NavLink to="/" className={header.navlink} onClick={this.logOut}>Выйти</NavLink>
-										: <NavLink to="/login" className={header.navlink} activeClassName={header.active}>Войти</NavLink>}
-								</li>
-							</ul>
-						</nav>
-					</div>
+const Header = (props) => {
+	const { isLogged, logOut } = props;
+	return (
+		<header>
+			<div className={header.layout}>
+				<div className={header.container}>
+					<nav className={header.nav}>
+						<ul className={header.nav_items}>
+							<li>
+								<NavLink to="/books" className={header.navlink} activeClassName={header.active}>Книги</NavLink>
+							</li>
+							<li>
+								<NavLink to="/authors" className={header.navlink} activeClassName={header.active}>Авторы</NavLink>
+							</li>
+							<li>
+								<NavLink to="/profile" className={header.navlink} activeClassName={header.active}>Профиль</NavLink>
+							</li>
+							<li>
+								{isLogged ? <NavLink to="/" className={header.navlink} onClick={logOut}>Выйти</NavLink>
+									: <NavLink to="/login" className={header.navlink} activeClassName={header.active}>Войти</NavLink>}
+							</li>
+						</ul>
+					</nav>
 				</div>
-			</header>
-		);
-	}
-}
+			</div>
+		</header>
+	);
+};
 
 Header.propTypes = {
 	isLogged: PropTypes.bool.isRequired,
